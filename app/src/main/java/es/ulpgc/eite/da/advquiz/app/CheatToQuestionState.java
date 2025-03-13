db6c591d9ec3b6d32549bd4f14619223b0e2bbcb
anaza <anazaretg@gmail.com>
2025-03-13 09:26:56 +0000

    13/03
    Implementamos las clases del cambio de estado "CheatToQuestion" y "QuestionToCheat"

--- app/src/main/java/es/ulpgc/eite/da/advquiz/app/CheatToQuestionState.java
+++ app/src/main/java/es/ulpgc/eite/da/advquiz/app/CheatToQuestionState.java
@@ -3,4 +3,10 @@ package es.ulpgc.eite.da.advquiz.app;
 public class CheatToQuestionState {
 
   // TODO: include code if necessary
+
+    public boolean cheated;
+
+    public CheatToQuestionState(Boolean cheated) {
+        this.cheated = cheated;
+    }
 }
--- app/src/main/java/es/ulpgc/eite/da/advquiz/app/QuestionToCheatState.java
+++ app/src/main/java/es/ulpgc/eite/da/advquiz/app/QuestionToCheatState.java
@@ -3,4 +3,11 @@ package es.ulpgc.eite.da.advquiz.app;
 public class QuestionToCheatState {
 
   // TODO: include code if necessary
+
+    public String answer;
+
+    public QuestionToCheatState(String answer) {
+        this.answer = answer;
+    }
+
 }
